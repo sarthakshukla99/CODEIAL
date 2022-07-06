495a8d643c2618c0fa1ab45861819acb00abe8fd
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-07-06 15:12:20 +0530

    connected to db , created schemas and models, imported ejs layout, created user sign up nd sign in pages, added them in controller and routes and done with sign up functionality for adding new user

---
+++ config/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development').
+then(()=>console.log('connection succesful')).catch((err)=>console.log('error in connection=>',err));
+
+
+// module.exports = db;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,61 @@
+const User = require('../models/user')
+
 module.exports.profile = function(req,res){
     return res.render('user_profile',{
         title: 'User Profile'
     });
+}
+
+// render the sing up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: 'Sign Up'
+    })
+}
+
+// render the sing in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: 'Sign In'
+    })
+}
+
+
+// get the sign up data
+module.exports.create = function(req,res){
+    // check if the pass and c_pass are different
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back')
+    }
+    // find for the user in db
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding the user=>',err);
+            return
+        }
+        // if user is not present in db
+        if(!user){
+            // create the user
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('error in signin up the user=>',err);
+                    return
+                }
+                // if no err in creating the user then redirct to sign-in page
+                return res.redirect('/users/sign-in');
+            })
+
+        // else if there is already a user with the same details then go back to sign-up page
+        }else{
+            return res.redirect('back');
+        }
+        
+        
+    })
+
+}
+
+
+module.exports.createSession = function(req,res){
+    //todo later
 }
