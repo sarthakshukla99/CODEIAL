00a4a2071e7ed0ed5e7b2be8371973d551c11fac
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-07-12 23:41:42 +0530

    added 2 level authentication for creating post

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create);
+router.post('/create', passport.checkAuthentication ,postController.create);
 
 
 module.exports = router;
