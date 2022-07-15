193035616c2951676f368794675acd80b4f743ee
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-07-15 21:26:18 +0530

    added update Profile feature in Profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 // module.exports.actionName = func(req,res){}
 const Posts = require('../models/post');
+const User = require('../models/user');
+
 module.exports.home = function(req,res){
     // populate the user/author of each post
     Posts.find({})
@@ -16,10 +18,15 @@ module.exports.home = function(req,res){
             return
         }
         else{
-            return res.render('home',{
-                title: 'Codeial | Home',
-                posts: posts
+            User.find({}, function(err,users){
+                return res.render('home',{
+                    title: 'Codeial | Home',
+                    posts: posts,
+                    all_users: users
+                })
+
             })
+
 
         }
     })
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,27 @@
 const User = require('../models/user')
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: 'User Profile'
-    });
+    User.findById(req.params.id, function(err,user){
+        return res.render('user_profile',{
+            title: 'User Profile',
+            profile_user: user
+        });
+    })
 }
+
+
+// update the profile page 
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        })
+    }else{
+        return res.status(401).send("Unauthorized");
+    }
+}
+
+
 
 // render the sing up page
 module.exports.signUp = function(req,res){
